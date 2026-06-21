b80bffb4654d39d4f3d9a4a8a24956b27e950547
giuliopons <pons@rockit.it>
2026-06-21 11:19:00 +0200

    fixed toggle labels in jobs

--- src/componenti/tsjob/ajax/toggleStato.php
+++ src/componenti/tsjob/ajax/toggleStato.php
@@ -12,8 +12,8 @@ $op = (integer)$_GET['op'];
 if($op!=1 && $op!=0) die("errore");
 $out = $obj->toggleStato($id,$op);
 $ar = array(
-    "0"=>"<a class='labelred' href=\"javascript:;\" onclick=\"setStato(this,'1',".$id.")\">CHIUSO</a>",
-    "1"=>"<a class='labelgreen' href=\"javascript:;\" onclick=\"setStato(this,'0',".$id.")\">APERTO</a>"
+    "0"=>"<a class='labelred' href=\"javascript:;\" onclick=\"setStato(this,'1',".$id.")\">{OFF}</a>",
+    "1"=>"<a class='labelgreen' href=\"javascript:;\" onclick=\"setStato(this,'0',".$id.")\">{ON}</a>"
 );
-echo $ar[$op];
+echo translateHtml( $ar[$op] );
 ?>
